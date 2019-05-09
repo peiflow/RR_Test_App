ffe3d4710005497142955cc43ead8830af564039
peiflow <palvarezg.work@outlook.es>
2019-05-09 20:21:50 +0200

    Button for open maps route in Google Maps throught a link

--- app/src/main/java/com/asociacion/rr_test_app/EventDetailsActivity.kt
+++ app/src/main/java/com/asociacion/rr_test_app/EventDetailsActivity.kt
@@ -1,7 +1,10 @@
 package com.asociacion.rr_test_app
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
 import android.widget.TextView
 import com.asociacion.rr_test_app.model.Event
 import com.asociacion.rr_test_app.model.LatLng
@@ -25,29 +28,16 @@ class EventDetailsActivity : AppCompatActivity(), OnMapReadyCallback {
             .findFragmentById(R.id.mapFragment1) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
+        val opnMapsBtn: Button = findViewById(R.id.open_maps_button)
+
         val evt = intent.getExtras().getSerializable("Event") as Event
+        processMapInfo(evt)
 
-        val eventTxt: TextView = findViewById(R.id.EventTxt)
-        val descTxt: TextView = findViewById(R.id.DescTxt)
-        val dateTxt: TextView = findViewById(R.id.DateTxt)
-        val timeTxt: TextView = findViewById(R.id.TimeTxt)
-        val locTxt: TextView = findViewById(R.id.LocationTxt)
-
-
-        val fmt = SimpleDateFormat("dd/MM/yyyy hh:mm:ss")
-        val date: Date = fmt.parse(evt.dateTime)
-        val fmtOut = SimpleDateFormat("dd/MM/yyyy")
-        val frmtDate: String = fmtOut.format(date)
-        val timeOut = SimpleDateFormat("hh:mm:ss")
-        val frmtTime: String = timeOut.format(date)
-
-        eventTxt.text = evt.name
-        descTxt.text = evt.description
-        dateTxt.text = frmtDate
-        timeTxt.text = frmtTime
-        locTxt.text = evt.address
-
-        markers = evt.location!!
+        opnMapsBtn.setOnClickListener{
+            val openUrl = Intent(Intent.ACTION_VIEW)
+            openUrl.data = Uri.parse("https://www.google.es/maps/dir/Toro+(Zamora),+Toro/47530+San+Román+de+Hornija,+Valladolid/Castronuño/La+Bóveda+de+Toro/Venialbo/@41.433834,-5.5421648,11z/data=!3m1!4b1!4m32!4m31!1m5!1m1!1s0xd38959c132a704f:0xa2b7a1b491bfb470!2m2!1d-5.3913375!2d41.5274417!1m5!1m1!1s0xd389620bacd852d:0x797ca0d3abb920fc!2m2!1d-5.2847709!2d41.480658!1m5!1m1!1s0xd38bb76f5686111:0x9bf614a77db387a6!2m2!1d-5.2671378!2d41.387238!1m5!1m1!1s0xd38c79765e54131:0xd2d704f21b666fe2!2m2!1d-5.4109725!2d41.3430855!1m5!1m1!1s0xd38c361b0eaff17:0x789cd4ef0164bb81!2m2!1d-5.5367936!2d41.3895764!3e0")
+            startActivity(openUrl)
+        }
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -93,5 +83,29 @@ class EventDetailsActivity : AppCompatActivity(), OnMapReadyCallback {
         }
         val mapSettings = mMap.uiSettings
         mapSettings.setAllGesturesEnabled(true)
+    }
+
+    private fun processMapInfo(evt:Event)
+    {
+        val eventTxt: TextView = findViewById(R.id.EventTxt)
+        val descTxt: TextView = findViewById(R.id.DescTxt)
+        val dateTxt: TextView = findViewById(R.id.DateTxt)
+        val timeTxt: TextView = findViewById(R.id.TimeTxt)
+        val locTxt: TextView = findViewById(R.id.LocationTxt)
+
+        val fmt = SimpleDateFormat("dd/MM/yyyy hh:mm:ss")
+        val date: Date = fmt.parse(evt.dateTime)
+        val fmtOut = SimpleDateFormat("dd/MM/yyyy")
+        val frmtDate: String = fmtOut.format(date)
+        val timeOut = SimpleDateFormat("hh:mm:ss")
+        val frmtTime: String = timeOut.format(date)
+
+        eventTxt.text = evt.name
+        descTxt.text = evt.description
+        dateTxt.text = frmtDate
+        timeTxt.text = frmtTime
+        locTxt.text = evt.address
+
+        markers = evt.location!!
     }
 }
